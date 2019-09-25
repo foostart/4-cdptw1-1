6c2ba08545ba018ee29052b1ad3000c5dde2e5a0
nguyenminhcanh <canhminh199x@gmail.com>
2019-09-25 10:51:48 +0700

    04

--- 1005/1005.php
+++ 1005/1005.php
@@ -1,3 +1,20 @@
+<?php
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+
+if (!class_exists('lessc')) {
+$dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
+require_once($dir_block . '/libs/lessc.inc.php');
+}
+
+$less = new lessc;
+$less->compileFile('less/styles.less', 'css/styles.less');
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -10,14 +27,14 @@
         $less = new lessc;
         $less->compileFile('less/styles.less', 'css/styles.css');
         ?>
-        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
-        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-        <link href="css/docs.theme.min.css" rel="stylesheet" type="text/css"/>
-        <link href="css/owl.carousel.min.css" rel="stylesheet" type="text/css"/>
-        <link href="css/owl.theme.default.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $url_path ?>/css/styles.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $url_path ?>/css/docs.theme.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $url_path ?>/css/owl.carousel.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $url_path ?>/css/owl.theme.default.min.css" rel="stylesheet" type="text/css"/>
         
-        <script src="js/jquery.min.js" type="text/javascript"></script>
-        <script src="js/owl.carousel.js" type="text/javascript"></script>
+        <script src="<?php echo $url_path ?>/js/jquery.min.js" type="text/javascript"></script>
+        <script src="<?php echo $url_path ?>/js/owl.carousel.js" type="text/javascript"></script>
     </head>
     <body>
       <?php include './1005-content.php'; ?>
--- 1006/1006.php
+++ 1006/1006.php
@@ -1,3 +1,20 @@
+<?php
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+
+if (!class_exists('lessc')) {
+$dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
+require_once($dir_block . '/libs/lessc.inc.php');
+}
+
+$less = new lessc;
+$less->compileFile('less/styles.less', 'css/styles.less');
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -11,12 +28,12 @@
         $less->compileFile('less/styles.less', 'css/styles.css');
         ?>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
-        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>   
-        <script src="js/jquery.min.js" type="text/javascript"></script>
-        <script src="js/bootstrap.min.js" type="text/javascript"></script>
-        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-        <script src="js/scripst.js" type="text/javascript"></script>
+        <link href="<?php echo $url_path ?>/css/styles.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>   
+        <script src="<?php echo $url_path ?>/js/jquery.min.js" type="text/javascript"></script>
+        <script src="<?php echo $url_path ?>/js/bootstrap.min.js" type="text/javascript"></script>
+        <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <script src="<?php echo $url_path ?>/js/scripst.js" type="text/javascript"></script>
     </head>
     <body>
         <?php include './1006-content.php'; ?>
